3c0f0ff062ce919038a0fb26f63c837d6a4b1597
Arun Jacob <arun.jacob@oracle.com>
2023-04-12 10:25:33 +0530

    More refactor

--- hello_test.go
+++ hello_test.go
@@ -3,11 +3,24 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Arun")
-	want := "Hello, Arun"
 
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Arun")
+		want := "Hello, Arun"
+		assertMessage(t, got, want)
+	})
+
+	t.Run("'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+		assertMessage(t, got, want)
+	})
+
+}
+
+func assertMessage(t testing.TB, got, want string) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
-
 }
